d3d44b17679b2e1f117d72f41f97641502b0c1d3
Adam <stargue49@gmail.com>
2020-10-24 12:28:59 +0800

    Updates

---
+++ precache-manifest.659dacdf5a66ddc94b79f187def05f59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a4f93827a723290e6c951c4a9d1fd6",
+    "url": "/MovieReactApp/index.html"
+  },
+  {
+    "revision": "883def549342d290d2f8",
+    "url": "/MovieReactApp/static/css/main.3931bf15.chunk.css"
+  },
+  {
+    "revision": "30159bfa50b82946d4ff",
+    "url": "/MovieReactApp/static/js/2.c3b48189.chunk.js"
+  },
+  {
+    "revision": "c897be67765481464f9e500ed505cd42",
+    "url": "/MovieReactApp/static/js/2.c3b48189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "883def549342d290d2f8",
+    "url": "/MovieReactApp/static/js/main.a2642efb.chunk.js"
+  },
+  {
+    "revision": "6ba338c31e54b18c19b7",
+    "url": "/MovieReactApp/static/js/runtime-main.fc438e6e.js"
+  }
+]);
